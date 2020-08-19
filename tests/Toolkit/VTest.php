82d92ec8acbd2bcf45ed81bddfd9c0d0a77cce54
Ahmet Bora <byybora@gmail.com>
2020-08-20 00:31:46 +0300

    Fix validation test #2777
    The defined rules put into the test have translation in the language file: `error.validation.same.RULENAME`. Only custom rules translation is not included in the language file.

--- tests/Toolkit/VTest.php
+++ tests/Toolkit/VTest.php
@@ -40,6 +40,26 @@ class VTest extends TestCase
         $this->assertFalse(V::me('you'));
     }
 
+    public function testCustomValue()
+    {
+        V::$validators['me'] = function ($name): bool {
+            return V::in($name, ['I', 'me', 'myself']);
+        };
+
+        $result = V::value('myself', [
+            'me' => ['I', 'me', 'myself']
+        ]);
+
+        $this->assertTrue($result);
+
+        $this->expectException('Exception');
+        $this->expectExceptionMessage('The "me" validation failed');
+
+        V::value('you', [
+            'me' => ['I', 'me', 'myself']
+        ]);
+    }
+
     public function testInvalidMethod()
     {
         $this->expectException('Exception');
@@ -514,7 +534,7 @@ class VTest extends TestCase
                     ]
                 ],
                 false,
-                'The "email" validation failed for field "email"',
+                'Please enter a valid email address for field "email"',
             ],
             // missing required field
             [
@@ -558,9 +578,9 @@ class VTest extends TestCase
     public function testValueFails()
     {
         $this->expectException('Exception');
-        $this->expectExceptionMessage('The "same" validation failed');
+        $this->expectExceptionMessage('Please enter "b"');
 
-        $result = V::value('a', [
+        V::value('a', [
             'same' => 'b'
         ]);
     }
